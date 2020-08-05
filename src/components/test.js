169e2f9ff8deea9164a362f1630f2340270ce6a9
mazenongithub <mazen@egeotechnical.com>
2020-08-05 12:24:22 -0700

    update to svg

--- src/components/test.js
+++ src/components/test.js
@@ -1,8 +1,131 @@
-return(<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 397.67 2402"><defs><style>.cls-1{font-size:18px;font-family:NunitoSans-Regular, Nunito Sans;}.cls-2{fill:none;stroke:#000;stroke-width:2px;}.cls-2,.cls-3{stroke-miterlimit:10;}.cls-3{fill:#6bb9f0;stroke:#231f20;}</style></defs><g id="Layer_2" data-name="Layer 2">
-<g id="hide"><g id="Layer_1-2" data-name="Layer 1"><text class="cls-1" transform="translate(2.72 2394.74)">12:00 am</text><rect class="cls-2" x="1" y="1" width="80" height="25"/><rect class="cls-2" x="1" y="26" width="80" height="25"/><rect class="cls-2" x="1" y="51" width="80" height="25"/><rect class="cls-2" x="1" y="76" width="80" height="25"/><rect class="cls-2" x="1" y="101" width="80" height="25"/><rect class="cls-2" x="1" y="126" width="80" height="25"/><rect class="cls-2" x="1" y="151" width="80" height="25"/><rect class="cls-2" x="1" y="176" width="80" height="25"/><rect class="cls-2" x="1" y="201" width="80" height="25"/><rect class="cls-2" x="1" y="226" width="80" height="25"/><rect class="cls-2" x="1" y="251" width="80" height="25"/><rect class="cls-2" x="1" y="276" width="80" height="25"/><rect class="cls-2" x="1" y="301" width="80" height="25"/><rect class="cls-2" x="1" y="326" width="80" height="25"/><rect class="cls-2" x="1" y="351" width="80" height="25"/><rect class="cls-2" x="1" y="376" width="80" height="25"/><rect class="cls-2" x="1" y="401" width="80" height="25"/><rect class="cls-2" x="1" y="426" width="80" height="25"/><rect class="cls-2" x="1" y="451" width="80" height="25"/><rect class="cls-2" x="1" y="476" width="80" height="25"/><rect class="cls-2" x="1" y="501" width="80" height="25"/>
-<rect class="cls-2" x="1" y="526" width="80" height="25"/><rect class="cls-2" x="1" y="551" width="80" height="25"/><rect class="cls-2" x="1" y="576" width="80" height="25"/><rect class="cls-2" x="1" y="601" width="80" height="25"/><rect class="cls-2" x="1" y="626" width="80" height="25"/><rect class="cls-2" x="1" y="651" width="80" height="25"/>
-<rect class="cls-2" x="1" y="676" width="80" height="25"/><rect class="cls-2" x="1" y="701" width="80" height="25"/><rect class="cls-2" x="1" y="726" width="80" height="25"/><rect class="cls-2" x="1" y="751" width="80" height="25"/><rect class="cls-2" x="1" y="776" width="80" height="25"/><rect class="cls-2" x="1" y="801" width="80" height="25"/><rect class="cls-2" x="1" y="826" width="80" height="25"/>
-<rect class="cls-2" x="1" y="851" width="80" height="25"/><rect class="cls-2" x="1" y="876" width="80" height="25"/><rect class="cls-2" x="1" y="901" width="80" height="25"/><rect class="cls-2" x="1" y="926" width="80" height="25"/><rect class="cls-2" x="1" y="951" width="80" height="25"/><rect class="cls-2" x="1" y="976" width="80" height="25"/><rect class="cls-2" x="1" y="1001" width="80" height="25"/><rect class="cls-2" x="1" y="1026" width="80" height="25"/><rect class="cls-2" x="1" y="1051" width="80" height="25"/><rect class="cls-2" x="1" y="1076" width="80" height="25"/><rect class="cls-2" x="1" y="1101" width="80" height="25"/><rect class="cls-2" x="1" y="1126" width="80" height="25"/><rect class="cls-2" x="1" y="1151" width="80" height="25"/><rect class="cls-2" x="1" y="1176" width="80" height="25"/><rect class="cls-2" x="1" y="1201" width="80" height="25"/><rect class="cls-2" x="1" y="1226" width="80" height="25"/><rect class="cls-2" x="1" y="1251" width="80" height="25"/><rect class="cls-2" x="1" y="1276" width="80" height="25"/><rect class="cls-2" x="1" y="1301" width="80" height="25"/><rect class="cls-2" x="1" y="1326" width="80" height="25"/><rect class="cls-2" x="1" y="1351" width="80" height="25"/><rect class="cls-2" x="1" y="1376" width="80" height="25"/><rect class="cls-2" x="1" y="1401" width="80" height="25"/><rect class="cls-2" x="1" y="1426" width="80" height="25"/><rect class="cls-2" x="1" y="1451" width="80" height="25"/><rect class="cls-2" x="1" y="1476" width="80" height="25"/><rect class="cls-2" x="1" y="1501" width="80" height="25"/><rect class="cls-2" x="1" y="1526" width="80" height="25"/><rect class="cls-2" x="1" y="1551" width="80" height="25"/><rect class="cls-2" x="1" y="1576" width="80" height="25"/>
-<rect class="cls-2" x="1" y="1601" width="80" height="25"/><rect class="cls-2" x="1" y="1626" width="80" height="25"/><rect class="cls-2" x="1" y="1651" width="80" height="25"/><rect class="cls-2" x="1" y="1676" width="80" height="25"/><rect class="cls-2" x="1" y="1701" width="80" height="25"/><rect class="cls-2" x="1" y="1726" width="80" height="25"/><rect class="cls-2" x="1" y="1751" width="80" height="25"/><rect class="cls-2" x="1" y="1776" width="80" height="25"/><rect class="cls-2" x="1" y="1801" width="80" height="25"/><rect class="cls-2" x="1" y="1826" width="80" height="25"/><rect class="cls-2" x="1" y="1851" width="80" height="25"/><rect class="cls-2" x="1" y="1876" width="80" height="25"/><rect class="cls-2" x="1" y="1901" width="80" height="25"/><rect class="cls-2" x="1" y="1926" width="80" height="25"/><rect class="cls-2" x="1" y="1951" width="80" height="25"/><rect class="cls-2" x="1" y="1976" width="80" height="25"/><rect class="cls-2" x="1" y="2001" width="80" height="25"/><rect class="cls-2" x="1" y="2026" width="80" height="25"/><rect class="cls-2" x="1" y="2051" width="80" height="25"/><rect class="cls-2" x="1" y="2076" width="80" height="25"/><rect class="cls-2" x="1" y="2101" width="80" height="25"/><rect class="cls-2" x="1" y="2126" width="80" height="25"/><rect class="cls-2" x="1" y="2151" width="80" height="25"/><rect class="cls-2" x="1" y="2176" width="80" height="25"/><rect class="cls-2" x="1" y="2201" width="80" height="25"/><rect class="cls-2" x="1" y="2226" width="80" height="25"/><rect class="cls-2" x="1" y="2251" width="80" height="25"/><rect class="cls-2" x="1" y="2276" width="80" height="25"/><rect class="cls-2" x="1" y="2301" width="80" height="25"/><rect class="cls-2" x="1" y="2326" width="80" height="25"/><rect class="cls-2" x="1" y="2351" width="80" height="25"/><rect class="cls-2" x="1" y="2376" width="80" height="25"/><text class="cls-1" transform="translate(8.12 95.45)">1:00 am</text><text class="cls-1" transform="translate(8.12 195.47)">2:00 am</text><text class="cls-1" transform="translate(8.12 295.48)">3:00 am</text><text class="cls-1" transform="translate(8.12 395.47)">4:00 am</text><text class="cls-1" transform="translate(8.12 497.41)">5:00 am</text><text class="cls-1" transform="translate(8.13 592.08)">6:00 am</text>
-<text class="cls-1" transform="translate(8.12 695.47)">7:00 am</text><text class="cls-1" transform="translate(8.12 795.47)">8:00 am</text><text class="cls-1" transform="translate(8.12 895.47)">9:00 am</text><text class="cls-1" transform="translate(2.72 992.08)">10:00 am</text><text class="cls-1" transform="translate(2.72 1095.47)">11:00 am</text><text class="cls-1" transform="translate(2.25 1195.47)">12:00 pm</text><text class="cls-1" transform="translate(7.65 1294.72)">1:00 pm</text><text class="cls-1" transform="translate(7.65 1394.74)">2:00 pm</text><text class="cls-1" transform="translate(7.65 1494.76)">3:00 pm</text><text class="cls-1" transform="translate(7.65 1594.74)">4:00 pm</text><text class="cls-1" transform="translate(7.65 1696.68)">5:00 pm</text><text class="cls-1" transform="translate(7.66 1791.35)">6:00 pm</text><text class="cls-1" transform="translate(7.65 1894.74)">7:00 pm</text><text class="cls-1" transform="translate(7.65 1994.74)">8:00 pm</text><text class="cls-1" transform="translate(7.65 2094.74)">9:00 pm</text><text class="cls-1" transform="translate(2.25 2191.35)">10:00 pm</text><text class="cls-1" transform="translate(2.25 2294.74)">11:00 pm</text>
-</g><rect class="cls-3" x="99" y="1" width="298.16" height="800"/></g></g></svg>)
+return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 397.67 2402">
+
+        <g id="hide">
+            <g id="Layer_1-2" data-name="Layer 1">
+              
+                <rect class="cls-2" x="1" y="1" width="80" height="25" />
+                <rect class="cls-2" x="1" y="26" width="80" height="25" />
+                <rect class="cls-2" x="1" y="51" width="80" height="25" />
+                <rect class="cls-2" x="1" y="76" width="80" height="25" />
+                <rect class="cls-2" x="1" y="101" width="80" height="25" />
+                <rect class="cls-2" x="1" y="126" width="80" height="25" />
+                <rect class="cls-2" x="1" y="151" width="80" height="25" />
+                <rect class="cls-2" x="1" y="176" width="80" height="25" />
+                <rect class="cls-2" x="1" y="201" width="80" height="25" />
+                <rect class="cls-2" x="1" y="226" width="80" height="25" />
+                <rect class="cls-2" x="1" y="251" width="80" height="25" />
+                <rect class="cls-2" x="1" y="276" width="80" height="25" />
+                <rect class="cls-2" x="1" y="301" width="80" height="25" />
+                <rect class="cls-2" x="1" y="326" width="80" height="25" />
+                <rect class="cls-2" x="1" y="351" width="80" height="25" />
+                <rect class="cls-2" x="1" y="376" width="80" height="25" />
+                <rect class="cls-2" x="1" y="401" width="80" height="25" />
+                <rect class="cls-2" x="1" y="426" width="80" height="25" />
+                <rect class="cls-2" x="1" y="451" width="80" height="25" />
+                <rect class="cls-2" x="1" y="476" width="80" height="25" />
+                <rect class="cls-2" x="1" y="501" width="80" height="25" />
+                <rect class="cls-2" x="1" y="526" width="80" height="25" />
+                <rect class="cls-2" x="1" y="551" width="80" height="25" />
+                <rect class="cls-2" x="1" y="576" width="80" height="25" />
+                <rect class="cls-2" x="1" y="601" width="80" height="25" />
+                <rect class="cls-2" x="1" y="626" width="80" height="25" />
+                <rect class="cls-2" x="1" y="651" width="80" height="25" />
+                <rect class="cls-2" x="1" y="676" width="80" height="25" />
+                <rect class="cls-2" x="1" y="701" width="80" height="25" />
+                <rect class="cls-2" x="1" y="726" width="80" height="25" />
+                <rect class="cls-2" x="1" y="751" width="80" height="25" />
+                <rect class="cls-2" x="1" y="776" width="80" height="25" />
+                <rect class="cls-2" x="1" y="801" width="80" height="25" />
+                <rect class="cls-2" x="1" y="826" width="80" height="25" />
+                <rect class="cls-2" x="1" y="851" width="80" height="25" />
+                <rect class="cls-2" x="1" y="876" width="80" height="25" />
+                <rect class="cls-2" x="1" y="901" width="80" height="25" />
+                <rect class="cls-2" x="1" y="926" width="80" height="25" />
+                <rect class="cls-2" x="1" y="951" width="80" height="25" />
+                <rect class="cls-2" x="1" y="976" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1001" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1026" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1051" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1076" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1101" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1126" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1151" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1176" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1201" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1226" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1251" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1276" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1301" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1326" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1351" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1376" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1401" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1426" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1451" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1476" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1501" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1526" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1551" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1576" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1601" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1626" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1651" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1676" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1701" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1726" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1751" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1776" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1801" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1826" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1851" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1876" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1901" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1926" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1951" width="80" height="25" />
+                <rect class="cls-2" x="1" y="1976" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2001" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2026" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2051" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2076" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2101" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2126" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2151" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2176" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2201" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2226" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2251" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2276" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2301" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2326" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2351" width="80" height="25" />
+                <rect class="cls-2" x="1" y="2376" width="80" height="25" />
+
+                <text class="cls-1" transform="translate(8.12 95.45)">1:00 am</text>
+                <text class="cls-1" transform="translate(8.12 195.47)">2:00 am</text>
+                <text class="cls-1" transform="translate(8.12 295.48)">3:00 am</text>
+                <text class="cls-1" transform="translate(8.12 395.47)">4:00 am</text>
+                <text class="cls-1" transform="translate(8.12 497.41)">5:00 am</text>
+                <text class="cls-1" transform="translate(8.13 592.08)">6:00 am</text>
+                <text class="cls-1" transform="translate(8.12 695.47)">7:00 am</text>
+                <text class="cls-1" transform="translate(8.12 795.47)">8:00 am</text>
+                <text class="cls-1" transform="translate(8.12 895.47)">9:00 am</text>
+                <text class="cls-1" transform="translate(2.72 992.08)">10:00 am</text>
+                <text class="cls-1" transform="translate(2.72 1095.47)">11:00 am</text>
+                <text class="cls-1" transform="translate(2.25 1195.47)">12:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1294.72)">1:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1394.74)">2:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1494.76)">3:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1594.74)">4:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1696.68)">5:00 pm</text>
+                <text class="cls-1" transform="translate(7.66 1791.35)">6:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1894.74)">7:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 1994.74)">8:00 pm</text>
+                <text class="cls-1" transform="translate(7.65 2094.74)">9:00 pm</text>
+                <text class="cls-1" transform="translate(2.25 2191.35)">10:00 pm</text>
+                <text class="cls-1" transform="translate(2.25 2294.74)">11:00 pm</text>
+                <text class="cls-1" transform="translate(2.72 2394.74)">12:00 am</text>
+                <rect class="cls-3" x="99" y="1" width="298.16" height="800" />
+            </g></g>
+    </svg>
+)
