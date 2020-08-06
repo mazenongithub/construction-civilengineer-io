49fd5cb43985118baa070b496eb1f7e1db04d00a
mazenongithub <mazen@egeotechnical.com>
2020-08-06 13:35:02 -0700

    updated test

--- src/components/test.js
+++ src/components/test.js
@@ -1,3 +1,33 @@
-treturn(<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 843.81 209.62"><defs><style>.cls-1{font-size:30px;}.cls-1,.cls-4{font-family:NunitoSans-SemiBold, Nunito Sans;font-weight:600;}.cls-2{letter-spacing:-0.01em;}.cls-3{letter-spacing:-0.08em;}.cls-4{font-size:40.31px;}.cls-5{letter-spacing:0em;}.cls-6{fill:#4939ea;}.cls-6,.cls-7{stroke:#000;stroke-miterlimit:10;}.cls-7{fill:none;stroke-width:2px;}</style></defs><g id="Layer_2" data-name="Layer 2"><g id="hide"><g id="Layer_1-2" data-name="Layer 1"><text class="cls-1" transform="translate(2.81 161.12)">Ma<tspan class="cls-2" x="41.28" y="0">z</tspan><tspan x="56.1" y="0">en Khenaisser</tspan></text><text class="cls-1" transform="translate(0 86.1)">Dump <tspan class="cls-3" x="90.96" y="0">T</tspan>
-<tspan x="106.8" y="0">ruck</tspan></text><text class="cls-4" transform="translate(40.09 37.66) scale(1.28 1)">M</text><text class="cls-4" transform="translate(167.69 37.63) scale(1.28 1)">T</text><text class="cls-4" transform="translate(274.76 37.66) scale(1.28 1)">W</text><text class="cls-4" transform="translate(527.54 37.66) scale(1.28 1)">F</text><text class="cls-4" transform="translate(387.48 37.66) scale(1.28 1)">TH</text><text class="cls-4" transform="translate(627.76 37.63) scale(1.28 1)"><tspan class="cls-5">S</tspan><tspan x="25.07" y="0">A</tspan></text><text class="cls-4" transform="translate(747.79 37.66) scale(1.28 1)">SU</text><rect class="cls-6" x="2.81" y="97.36" width="120" height="26.97"/><rect class="cls-6" x="2.81" y="182.15" width="120" height="26.97"/>
-<rect class="cls-7" x="2.81" y="1" width="120" height="50"/><rect class="cls-7" x="122.81" y="1" width="120" height="50"/><rect class="cls-7" x="242.81" y="1" width="120" height="50"/><rect class="cls-7" x="362.81" y="1" width="120" height="50"/><rect class="cls-7" x="482.81" y="1" width="120" height="50"/><rect class="cls-7" x="602.81" y="1" width="120" height="50"/><rect class="cls-7" x="722.81" y="1" width="120" height="50"/></g></g></g></svg>)
+treturn(
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 843.81 209.62">
+        <g id="Layer_2" data-name="Layer 2">
+            <g id="hide">
+                <g id="Layer_1-2" data-name="Layer 1">
+                   
+                    <text class="cls-1" transform="translate(0 86.1)">Dump <tspan class="cls-3" x="90.96" y="0">T</tspan><tspan x="106.8" y="0">ruck</tspan></text>
+                    <rect class="cls-6" x="2.81" y="97.36" width="120" height="26.97" />
+                    <text class="cls-1" transform="translate(2.81 161.12)">Ma<tspan class="cls-2" x="41.28" y="0">z</tspan><tspan x="56.1" y="0">en Khenaisser</tspan></text>
+                    <rect class="cls-6" x="2.81" y="182.15" width="120" height="26.97" />
+                    <text class="cls-4" transform="translate(40.09 37.66) scale(1.28 1)">M</text>
+                    <text class="cls-4" transform="translate(167.69 37.63) scale(1.28 1)">T</text>
+                    <text class="cls-4" transform="translate(274.76 37.66) scale(1.28 1)">W</text>
+                    <text class="cls-4" transform="translate(387.48 37.66) scale(1.28 1)">TH</text>
+                    <text class="cls-4" transform="translate(527.54 37.66) scale(1.28 1)">F</text>
+                    <text class="cls-4" transform="translate(627.76 37.63) scale(1.28 1)">SA</text>
+                    <text class="cls-4" transform="translate(747.79 37.66) scale(1.28 1)">SU</text>
+
+                    
+                
+
+                    <rect class="cls-7" x="2.81" y="1" width="120" height="50" />
+                    <rect class="cls-7" x="122.81" y="1" width="120" height="50" />
+                    <rect class="cls-7" x="242.81" y="1" width="120" height="50" />
+                    <rect class="cls-7" x="362.81" y="1" width="120" height="50" />
+                    <rect class="cls-7" x="482.81" y="1" width="120" height="50" />
+                    <rect class="cls-7" x="602.81" y="1" width="120" height="50" />
+                    <rect class="cls-7" x="722.81" y="1" width="120" height="50" />
+
+                </g>
+            </g>
+        </g>
+    </svg>)
