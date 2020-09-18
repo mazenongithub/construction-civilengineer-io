73e3974e3b7a9d3cd6693a2a15105eb4b6f0df2c
mazenongithub <mazen@egeotechnical.com>
2020-09-17 17:20:36 -0700

    actual fix

--- src/components/actual.js
+++ src/components/actual.js
@@ -467,12 +467,18 @@ class Actual extends Component {
                     const materialdateday = mymaterial.timein.substring(8, 10);
 
                     const csi = dynamicstyles.getcsibyid.call(this, mymaterial.csiid);
-                    const csi_1 = csi.csi.substring(0, 2)
-                    const csi_2 = csi.csi.substring(2, 4);
-                    const csi_3 = csi.csi.substring(4, 6);
+                    let csi_1 = "";
+                    let csi_2 = "";
+                    let csi_3 = "";
                     let csi_4 = "";
-                    if (csi.csi.length > 6) {
-                        csi_4 = csi.csi.substring(7, 9);
+                    if (csi) {
+                        csi_1 = csi.csi.substring(0, 2)
+                        csi_2 = csi.csi.substring(2, 4);
+                        csi_3 = csi.csi.substring(4, 6);
+
+                        if (csi.csi.length > 6) {
+                            csi_4 = csi.csi.substring(7, 9);
+                        }
                     }
 
                     this.setState({ materialdatemonth, materialdateday, materialdateyear, activematerialid: materialid, csi_1, csi_2, csi_3, csi_4 })
@@ -567,13 +573,21 @@ class Actual extends Component {
                     const timeoutampm = getAMPMfromTimeIn(mylabor.timeout);
 
                     const csi = dynamicstyles.getcsibyid.call(this, mylabor.csiid);
-                    const csi_1 = csi.csi.substring(0, 2)
-                    const csi_2 = csi.csi.substring(2, 4);
-                    const csi_3 = csi.csi.substring(4, 6);
+                    let csi_1 = "";
+                    let csi_2 = "";
+                    let csi_3 = "";
                     let csi_4 = "";
+                    if(csi) {
+                    csi_1 = csi.csi.substring(0, 2)
+                    csi_2 = csi.csi.substring(2, 4);
+                    csi_3 = csi.csi.substring(4, 6);
+                 
+                   
                     if (csi.csi.length > 6) {
                         csi_4 = csi.csi.substring(7, 9);
                     }
+
+                }
 
 
                     this.setState({ activelaborid: laborid, timeinmonth, timeinday, timeinyear, timeinhours, timeinminutes, timeinampm, timeoutmonth, timeoutday, timeoutyear, timeouthours, timeoutminutes, timeoutampm, csi_1, csi_2, csi_3, csi_4 })
